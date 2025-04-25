c1b7bd8011c0f3ebbc833ff4453869ec6cdbf7f6
tasselii <thiagotasseli@outlook.com.br>
2025-04-25 09:38:23 -0300

    :wrench: Adicionando README e Modificação

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -26,13 +26,13 @@ public class Menu {
 					System.out.println("║" + Cores.TEXT_WHITE + Cores.ANSI_BLACK_BACKGROUND + " 6 - Sacar                            " + Cores.TEXT_CYAN + "║");
 					System.out.println("║" + Cores.TEXT_WHITE + Cores.ANSI_BLACK_BACKGROUND + " 7 - Depositar                        " + Cores.TEXT_CYAN + "║");
 					System.out.println("║" + Cores.TEXT_WHITE + Cores.ANSI_BLACK_BACKGROUND + " 8 - Transferir                       " + Cores.TEXT_CYAN + "║");
-					System.out.println("║" + Cores.TEXT_WHITE + Cores.ANSI_BLACK_BACKGROUND + " 9 - Sair                             " + Cores.TEXT_CYAN + "║");
+					System.out.println("║" + Cores.TEXT_WHITE + Cores.ANSI_BLACK_BACKGROUND + Cores.TEXT_YELLOW +" 9 - Sair                             " + Cores.TEXT_CYAN + "║");
 					System.out.println("╚══════════════════════════════════════╝" + Cores.TEXT_RESET);
 
 					opcao = scan.nextInt();
 						
 					if (opcao == 9) {
-						System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+						System.out.println(Cores.TEXT_WHITE_BOLD + Cores.ANSI_BLACK_BACKGROUND + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
 						sobre();
 						scan.close();
 						System.exit(0);
@@ -54,14 +54,14 @@ public class Menu {
 	
 	public static void sobre() {
 		System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_BLACK_BACKGROUND +
-			    Cores.TEXT_CYAN + "\n╔═══════════════════════════════════════════════════╗");
-			System.out.println("║" + Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_BLACK_BACKGROUND + "  Projeto Desenvolvido por:                        " + Cores.TEXT_CYAN + "║");
+			    Cores.TEXT_CYAN + "\n╔════════════════════════════════════════════════════╗");
+			System.out.println("║" + Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_BLACK_BACKGROUND + "  Projeto Desenvolvido por:                        " + Cores.TEXT_CYAN + " ║");
 			System.out.println("║" + Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_BLACK_BACKGROUND + "  Thiago Tasseli - " +
 			    Cores.TEXT_BLUE_UNDERLINED + Cores.ANSI_BLACK_BACKGROUND + "tasselii.dev@outlook.com.br" +
-			    Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_BLACK_BACKGROUND + "     " + Cores.TEXT_CYAN + "║");
+			    Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_BLACK_BACKGROUND + "     " + Cores.TEXT_CYAN + " ║");
 			System.out.println("║  " + Cores.TEXT_BLUE_UNDERLINED + Cores.ANSI_BLACK_BACKGROUND +
-			    "github.com/tasselii" + Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_BLACK_BACKGROUND + "                              " + Cores.TEXT_CYAN + "║");
-			System.out.println("╚═══════════════════════════════════════════════════╝" + Cores.TEXT_RESET);
+			    "github.com/tasselii" + Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_BLACK_BACKGROUND + "                              " + Cores.TEXT_CYAN + " ║");
+			System.out.println("╚════════════════════════════════════════════════════╝" + Cores.TEXT_RESET);
 
 	}
 }
