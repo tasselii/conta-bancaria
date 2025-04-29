9d4dff5c9a13d7cf9e6971334ed574e756ddc5e1
tasselii <thiagotasseli@outlook.com.br>
2025-04-29 16:49:52 -0300

    :sparkles: Construção da Interface ContaRepository e da Classe ContaController

---
+++ src/conta_bancaria/controller/ContaController.java
@@ -0,0 +1,57 @@
+package conta_bancaria.controller;
+
+import conta_bancaria.model.Conta;
+import conta_bancaria.repository.ContaRepository;
+
+public class ContaController implements ContaRepository{
+
+	@Override
+	public void procurarPorNumero(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarTodas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cadastrar(Conta conta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Conta conta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletar(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sacar(int numero, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void depositar(int numero, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
---
+++ src/conta_bancaria/repository/ContaRepository.java
@@ -0,0 +1,19 @@
+package conta_bancaria.repository;
+
+import conta_bancaria.model.Conta;
+
+public interface ContaRepository {
+
+	// Métodos do CRUD (Creat, Read, Update, Delete)
+	public void procurarPorNumero(int numero);
+	public void listarTodas();
+	public void cadastrar(Conta conta);
+	public void atualizar(Conta conta);
+	public void deletar(int numero);
+	
+	// Métodos Bancários
+	public void sacar(int numero, float valor);
+	public void depositar(int numero, float valor);
+	public void transferir(int numeroOrigem, int numeroDestino, float valor);
+	
+}
